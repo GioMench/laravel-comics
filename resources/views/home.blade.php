eb883cdf7c118d6d3a12f97328a9cde8f8a8222b
GioMench <giorgiaglm@gmail.com>
2024-05-07 18:30:10 +0200

    fix margin of row card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="container container_card">
-        <div class="row row-cols-6 g-5 my-4">
+        <div class="row row-cols-6 g-5 my-0">
             @foreach ($comics as $comic)
             <div class="col p-3 my-5">
                 <div class="card" style="height:200px;border: none">
