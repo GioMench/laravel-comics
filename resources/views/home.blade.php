3ddb8c7ef9e3460113792d0381ef3b80231693d6
GioMench <giorgiaglm@gmail.com>
2024-05-07 17:51:06 +0200

    addd partials of sass

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,14 @@
 
     <div class="container">
         <div class="row">
-            @foreach ($comics as $comic )
+            @foreach ($comics as $comic)
             <div class="col">
-                {{$comic['title']}}
+                <div class="card" style="width: 18rem;">
+                    <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$comic['title']}}</h5>
+                    </div>
+                </div>
             </div>
             @endforeach
         </div>
