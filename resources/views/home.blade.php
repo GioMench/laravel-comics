f5274c5815e64d274f0b9c09f812c410b515dd0e
GioMench <giorgiaglm@gmail.com>
2024-05-07 18:28:01 +0200

    set card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
+    <div class="container container_card">
+        <div class="row row-cols-6 g-5 my-4">
             @foreach ($comics as $comic)
-            <div class="col">
-                <div class="card" style="width: 18rem;">
-                    <img src="{{$comic['thumb']}}" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title">{{$comic['title']}}</h5>
+            <div class="col p-3 my-5">
+                <div class="card" style="height:200px;border: none">
+                    <img src="{{$comic['thumb']}}" class="card-img-top" alt="..." style="height: 100%; width: 100%;">
+                    <div class="card-body" class="margin-bottom: 100px">
+                        <h6 class="card-title text-white text-uppercase">{{$comic['title']}}</h6>
                     </div>
                 </div>
             </div>
