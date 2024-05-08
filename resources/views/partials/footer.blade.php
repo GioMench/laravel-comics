0ef1f69c130e14c01bba9c18e65f113cdce43b84
GioMench <giorgiaglm@gmail.com>
2024-05-08 13:08:13 +0200

    set and style footer first section

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,35 +1,83 @@
 <footer>
     <section class="container-fluid footer_bg">
-        <div class="container d-flex">
-            <div class="text-light d-flex">
-                <div>
-                    <ul class="list-unstyled">
-                        <li>1</li>
-                        <li>2</li>
-                    </ul>
-                    <ul class="list-unstyled">
-                        <li>5</li>
-                        <li>9</li>
-                        <li>9</li>
-                        <li>9</li>
-                        <li>9</li>
-                    </ul>
-                </div>
-
-
-                <ul class="list-unstyled">
-                    <li>2</li>
-                </ul>
-
-                <ul class="list-unstyled">
-                    <li>4</li>
-                </ul>
-
-
-            </div>
+        <div class="container d-flex justify-content-between">
             <div>
-                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="" style="height: 100%">
+                <div class="text-light d-flex" style="padding-top: 50px">
+                    <div>
+                        <ul class="list-unstyled px-4">
+                            <li>
+                                <h5 class="text-uppercase fw-bolder">
+                                    dc comics
+                                </h5>
+                            </li>
+                            <li class="fw-lighter">Characters</li>
+                            <li class="fw-lighter">Comincs</li>
+                            <li class="fw-lighter">Movies</li>
+                            <li class="fw-lighter">TV</li>
+                            <li class="fw-lighter">Games</li>
+                            <li class="fw-lighter">Videos</li>
+                            <li class="fw-lighter">News</li>
+                        </ul>
+                        <ul class="list-unstyled px-4">
+                            <li>
+                                <h5 class="text-uppercase fw-bolder">
+                                    shop
+                                </h5>
+                            </li>
+                            <li class="fw-lighter">Shop DC</li>
+                            <li class="fw-lighter">Shop DC Colectibles</li>
+                        </ul>
+                    </div>
+
+
+                    <ul class="list-unstyled px-4">
+                        <li>
+                            <h5 class="text-uppercase fw-bolder">
+                                dc
+                            </h5>
+                        </li>
+                        <li class="fw-lighter">Terms Of Use</li>
+                        <li class="fw-lighter">Privacy policy (New)</li>
+                        <li class="fw-lighter">Ad Choices</li>
+                        <li class="fw-lighter">Advertising</li>
+                        <li class="fw-lighter">Jobs</li>
+                        <li class="fw-lighter">Subscriptions</li>
+                        <li class="fw-lighter">Talent Workshops</li>
+                        <li class="fw-lighter">CPSC Certificates</li>
+                        <li class="fw-lighter">Ratings</li>
+                        <li class="fw-lighter">Shop Help</li>
+                        <li class="fw-lighter">Contact Us</li>
+                    </ul>
+
+                    <ul class="list-unstyled px-4">
+                        <li>
+                            <h5 class="text-uppercase fw-bolder">
+                                sites
+                            </h5>
+                        </li>
+                        <li class="fw-lighter">DC</li>
+                        <li class="fw-lighter">MAD Magazine</li>
+                        <li class="fw-lighter">DC Kids</li>
+                        <li class="fw-lighter">DC Universe</li>
+                        <li class="fw-lighter">DC Power Visa</li>
+                    </ul>
+
+
+
+                </div>
+                <div class="text-light align-items-start align-self-end px-4 fw-lighter cookies">
+                    All Site Content TM and © 2020 DC
+                    Entertainment, unless otherwise <span class="text-primary">noted here</span>.
+                    All rights reserved.
+                    <span class="text-primary">Cookies Settings </span>
+                </div>
+            </div>
+
+            <div>
+
+                <img src="{{ Vite::asset('resources/img/dc-logo-bg.png') }}" alt="" style="width: 550px; height: 100%; margin-top: -40px">
             </div>
         </div>
+
     </section>
 </footer>
